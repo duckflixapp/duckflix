95f441d0d42ccf71ed409a28ced7821c6f30c38e
nikola04 <nikolanedeljkovic.official@gmail.com>
2026-02-23 01:52:46 +0000

    Fixed subtitles for Chrome Cast

--- src/shared/configs/cors.ts
+++ src/shared/configs/cors.ts
@@ -1,12 +1,14 @@
-import type { Request } from "express";
+import type { Request } from 'express';
 import cors from 'cors';
-import { env } from "bun";
+import { env } from 'bun';
 
 export const corsConfiguration = cors((req: Request, callback) => {
-    if (req.path.includes('/media/')) {
-        callback(null, { origin: '*' });
+    const requestOrigin = req.headers.origin;
+
+    if ((!requestOrigin || requestOrigin === 'https://www.gstatic.com') && req.path.includes('/media/')) {
+        callback(null, { origin: '*', credentials: false });
         return;
     }
-    
-    callback(null, { origin: env.ORIGIN, credentials: true })
+
+    callback(null, { origin: env.ORIGIN, credentials: true });
 });
--- src/shared/configs/helmet.ts
+++ src/shared/configs/helmet.ts
@@ -1,18 +1,18 @@
-import helmet from "helmet";
-import { env } from "../../env";
+import helmet from 'helmet';
+import { env } from '../../env';
 
 export const helmetConfiguration = helmet({
-contentSecurityPolicy: {
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-        imgSrc: ["'self'", 'data:', 'https://image.tmdb.org'],
-        mediaSrc: ["'self'", "blob:", "data:", env.BASE_URL, env.ORIGIN, "https://www.gstatic.com"],
-        connectSrc: ["'self'", env.BASE_URL, "https://www.gstatic.com"],
-        upgradeInsecureRequests: env.NODE_ENV === 'production' ? [] : null,
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+            imgSrc: ["'self'", 'data:', 'https://image.tmdb.org'],
+            mediaSrc: ["'self'", 'blob:', 'data:', env.BASE_URL, env.ORIGIN, 'https://www.gstatic.com'],
+            connectSrc: ["'self'", env.BASE_URL, env.ORIGIN, 'https://www.gstatic.com'],
+            upgradeInsecureRequests: env.NODE_ENV === 'production' ? [] : null,
+        },
     },
-},
-crossOriginResourcePolicy: { policy: 'cross-origin' },
-hidePoweredBy: true,
-crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+    hidePoweredBy: true,
+    crossOriginEmbedderPolicy: false,
 });
